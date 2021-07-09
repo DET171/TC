a032dbc74e13d5d6a48831aab1767474c1d61245
DET171 <mrgeek484@gmail.com>
2021-07-09 15:36:29 +0800

    deprecated

--- index.py
+++
@@ -1,9 +0,0 @@
-import discord
-from discord.ext import commands
-from pretty_help import Navigation, PrettyHelp
-
-from asyncio import sleep
-import os
-import random
-
-bot = commands.Bot(command_prefix="$", description="Simple economy bot")
